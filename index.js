d8e21e5cbfa516e6a2c81793fe9eb3dbb0d08c03
mdsujonislam666c <mdsujonislam666>
2025-12-09 23:32:36 +0600

    this code pushed for loan application payment method get operation /loanApplication/:id

--- index.js
+++ index.js
@@ -48,6 +48,13 @@ async function run() {
         res.send(result);
     })
 
+    app.get('/loanApplications/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await applicationCollection.findOne(query);
+      res.send(result);
+    })
+
     app.post('/loanApplications', async(req, res) =>{
         const application = req.body;
         application.createdAt = new Date();
